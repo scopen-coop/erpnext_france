43b073ab2a18bd2a4ecbb07ec27e032c80c94cc5
Florian HENRY <florian.henry@open-concept.pro>
2023-08-25 17:15:57 +0200

    review file location

--- erpnext_france/public/js/customer.js
+++ erpnext_france/public/js/customer.js
@@ -14,28 +14,3 @@ frappe.ui.form.on("Customer", "subledger_account", function(frm,cdt,cdn) {
     }
 
 });
-
-frappe.ui.form.on("Customer", {
-  custom_check_vat_id(frm) {
-    frappe.call({
-        method: "erpnext_france.check_vat.eu_vat.check_vat",
-        args: {vat_id: frm.doc.tax_id},
-        callback: function (r) {
-          if (r.message)
-            if (r.message.valid) {
-                frappe.msgprint({
-                    title: __('VAT Check OK'),
-                    indicator: 'green',
-                    message: __('Name: {0}, Adress:{1}',[r.message.name,r.message.address])
-                });
-            } else {
-                frappe.msgprint({
-                    title: __('Invalid VAT number'),
-                    indicator: 'red',
-                    message: __('Invalid VAT number')
-                });
-            }
-        }
-      })
-  },
-});
