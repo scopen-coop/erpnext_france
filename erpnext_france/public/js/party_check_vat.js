797ba8665c5bfe0582498bb69a27caf1011cd013
Florian HENRY <florian.henry@open-concept.pro>
2024-03-04 16:14:27 +0100

    add freeze disalog on VAT EU check

--- erpnext_france/public/js/party_check_vat.js
+++ erpnext_france/public/js/party_check_vat.js
@@ -2,59 +2,63 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Customer", {
-  check_vat_id(frm) {
-    frappe.call({
-      method: "erpnext_france.utils.eu_vat.check_vat",
-      args: {vat_id: frm.doc.tax_id},
-      callback: function (r) {
-        if (r.message)
-          if (r.message.valid) {
-            console.log(r.message)
-            frappe.msgprint({
-              title: __('VAT Check OK'),
-              indicator: 'green',
-              as_list: true,
-              message: [__('Name: {0}', [r.message.name]), __('Adress: {0}', [r.message.address]), __('Source: https://ec.europa.eu/taxation_customs/vies/#/vat-validation')]
-            });
-          } else {
-            frappe.msgprint({
-              title: __('Invalid VAT number'),
-              indicator: 'red',
-              as_list: true,
-              message: [__('Invalid VAT number'), __('Source: https://ec.europa.eu/taxation_customs/vies/#/vat-validation')]
-            });
-          }
-      }
-    })
-  },
+	check_vat_id(frm) {
+		frappe.msgprint()
+		frappe.call({
+			method: "erpnext_france.utils.eu_vat.check_vat",
+			args: {vat_id: frm.doc.tax_id},
+			freeze: true,
+			freeze_message: __('Check request sent to EU web site, please wait'),
+			callback: function (r) {
+				if (r.message)
+					if (r.message.valid) {
+						frappe.msgprint({
+							title: __('VAT Check OK'),
+							indicator: 'green',
+							as_list: true,
+							message: [__('Name: {0}', [r.message.name]), __('Adress: {0}', [r.message.address]), __('Source: https://ec.europa.eu/taxation_customs/vies/#/vat-validation')]
+						});
+					} else {
+						frappe.msgprint({
+							title: __('Invalid VAT number'),
+							indicator: 'red',
+							as_list: true,
+							message: [__('Invalid VAT number'), __('Source: https://ec.europa.eu/taxation_customs/vies/#/vat-validation')]
+						});
+					}
+			}
+		})
+	},
 });
 
 
 frappe.ui.form.on("Supplier", {
-  check_vat_id(frm) {
-    frappe.call({
-      method: "erpnext_france.utils.eu_vat.check_vat",
-      args: {vat_id: frm.doc.tax_id},
-      callback: function (r) {
-        if (r.message)
-          if (r.message.valid) {
-            frappe.msgprint({
-              title: __('VAT Check OK'),
-              indicator: 'green',
-              as_list: true,
-              message: [__('Name: {0}', [r.message.name]),
-                __('Adress: {0}', [r.message.address]),
-                __('Source:https://ec.europa.eu/taxation_customs/vies/#/vat-validation')]
-            });
-          } else {
-            frappe.msgprint({
-              title: __('Invalid VAT number'),
-              indicator: 'red',
-              as_list: true,
-              message: [__('Invalid VAT number'), __('Source:https://ec.europa.eu/taxation_customs/vies/#/vat-validation')]
-            });
-          }
-      }
-    })
-  },
+	check_vat_id(frm) {
+		frappe.call({
+			method: "erpnext_france.utils.eu_vat.check_vat",
+			args: {vat_id: frm.doc.tax_id},
+			freeze: true,
+			freeze_message: __('Check request sent to EU web site, please wait'),
+			callback: function (r) {
+				if (r.message)
+					if (r.message.valid) {
+						frappe.msgprint({
+							title: __('VAT Check OK'),
+							indicator: 'green',
+							as_list: true,
+							message: [__('Name: {0}', [r.message.name]),
+								__('Adress: {0}', [r.message.address]),
+								__('Source:https://ec.europa.eu/taxation_customs/vies/#/vat-validation')]
+						});
+					} else {
+						frappe.msgprint({
+							title: __('Invalid VAT number'),
+							indicator: 'red',
+							as_list: true,
+							message: [__('Invalid VAT number'), __('Source:https://ec.europa.eu/taxation_customs/vies/#/vat-validation')]
+						});
+					}
+			}
+		})
+	},
 });
