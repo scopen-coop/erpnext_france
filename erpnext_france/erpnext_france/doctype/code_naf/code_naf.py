4eb216dd79335bdbc4bff109473c3017c9fed782
Florian HENRY <florian.henry@open-concept.pro>
2024-08-27 10:42:02 +0200

    search naf an formjurique by code

--- erpnext_france/erpnext_france/doctype/code_naf/code_naf.py
+++ erpnext_france/erpnext_france/doctype/code_naf/code_naf.py
@@ -4,5 +4,17 @@
 # import frappe
 from frappe.model.document import Document
 
+
 class CodeNaf(Document):
-	pass
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
+
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from frappe.types import DF
+
+        code: DF.Data
+        label: DF.Data
+    # end: auto-generated types
+    pass
--- erpnext_france/erpnext_france/doctype/legal_form/legal_form.py
+++ erpnext_france/erpnext_france/doctype/legal_form/legal_form.py
@@ -4,5 +4,17 @@
 # import frappe
 from frappe.model.document import Document
 
+
 class LegalForm(Document):
-	pass
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
+
+    from typing import TYPE_CHECKING
+
+    if TYPE_CHECKING:
+        from frappe.types import DF
+
+        code: DF.Data | None
+        label: DF.Data | None
+    # end: auto-generated types
+    pass
