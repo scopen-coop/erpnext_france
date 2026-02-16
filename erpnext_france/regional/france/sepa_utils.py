c76039005d1e01c854089856272a8f1edd1bfa63
Dylan Bourdere <d.bourdere@gmail.com>
2026-02-16 14:57:01 +0100

    fix

--- erpnext_france/regional/france/sepa_utils.py
+++ erpnext_france/regional/france/sepa_utils.py
@@ -189,22 +189,8 @@ def reconcile_bank_transaction_to_sepa_line(bank_transaction, end_to_end_id):
 
 	# Get GL Account from Bank Account
 	gl_account = frappe.db.get_value("Bank Account", bordereau.bank_account, "account")
-	
 	if not gl_account:
-		# Fallback: Try to get account from Invoice -> Mode of Payment
-		# We pick the first invoice to determine the mode of payment, assuming they are consistent or we take the one from the line
-		sepa_inv = frappe.get_doc("Sales Invoice" if sepa_line.party_type == "Customer" else "Purchase Invoice", sepa_line.invoice)
-		mode_of_payment = sepa_inv.get("mode_of_payment")
-		if mode_of_payment:
-			# Get default account for this company and mode of payment
-			mode_of_payment_doc = frappe.get_doc("Mode of Payment", mode_of_payment)
-			for account_row in mode_of_payment_doc.accounts:
-				if account_row.company == bordereau.company:
-					gl_account = account_row.default_account
-					break
-
-	if not gl_account:
-		frappe.throw(_("Please link a GL Account to the Bank Account {0} or set a Default Account in Mode of Payment").format(bordereau.bank_account))
+		frappe.throw(_("Please link a GL Account to the Bank Account {0}").format(bordereau.bank_account))
 
 	# Create Payment Entry
 	payment_entry = frappe.get_doc({
