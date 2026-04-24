a0dca40a4eed6443a038666aa4c61d1fad70dd95
Florian HENRY <florian.henry@open-concept.pro>
2026-04-24 15:35:18 +0200

    feat: upgrade version

--- erpnext_france/__init__.py
+++ erpnext_france/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "15.0.5"
+__version__ = "15.0.6"
 # import erpnext.accounts.doctype.account.chart_of_accounts.chart_of_accounts
 
 # from erpnext_france.erpnext_france.overrides.doctype.chart_of_accounts import get_chart_fr
