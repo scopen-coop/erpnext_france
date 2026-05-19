88d4e5b777ec9085d925f49e352aebfeb2da2aa3
Nico <nicolas.lavoine@scopen.fr>
2026-05-19 14:49:20 +0200

    Feat: Added Patch for due_date_based_on_france field

---
+++ erpnext_france/patches/v15_0/migrate_due_date_based_on_france.py
@@ -0,0 +1,15 @@
+import frappe
+
+
+def execute():
+	frappe.db.sql(
+		"""
+        UPDATE `tabPayment Term`
+        SET `custom_due_date_based_on_france` = `due_date_based_on`
+        WHERE (`custom_due_date_based_on_france` IS NULL
+            OR `custom_due_date_based_on_france` = '')
+        AND (`due_date_based_on` IS NOT NULL
+            AND `due_date_based_on` != '')
+    """
+	)
+	frappe.db.commit()
