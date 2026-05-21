fdcb29049ece722717dedab932e0fa71d3ee336b
Florian HENRY <florian.henry@open-concept.pro>
2026-05-21 13:51:46 +0200

    fix: migrate with python

--- erpnext_france/patches/v15_0/migrate_due_date_based_on_france.py
+++ erpnext_france/patches/v15_0/migrate_due_date_based_on_france.py
@@ -2,13 +2,11 @@ import frappe
 
 
 def execute():
-	frappe.db.sql(
-		"""
-        UPDATE `tabPayment Term`
-        SET `custom_due_date_based_on_france` = `due_date_based_on`
-        WHERE (`custom_due_date_based_on_france` IS NULL
-            OR `custom_due_date_based_on_france` = '')
-        AND (`due_date_based_on` IS NOT NULL
-            AND `due_date_based_on` != '')
-    """
-	)
+	frappe.reload_doc("Accounts", "doctype", "Payment Term")
+	payment_terms = frappe.get_all("Payment Term")
+	for payment_term in payment_terms:
+		if payment_term.get("custom_due_date_based_on_france") is None and payment_term.get(
+			"due_date_based_on"
+		):
+			payment_term.custom_due_date_based_on_france = payment_term.due_date_based_on
+			payment_term.save()
